82b72866032d0ee2072cfa415676526651f2bcee
Mohammad-Gholizadeh <mgh007753@gmail.com>
2023-11-28 18:58:52 +0330

    part #23

--- assets/js/alpineJsControllers/usersController.js
+++ assets/js/alpineJsControllers/usersController.js
@@ -1,129 +1,169 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('usersData', ()=>({
-            mainUsers: [],
-            users: [],
-            pageUsers: [],
-            isLoading: false,
-            showAdModal: false,
-            pageCount: 1,
-            itemsCount: 4,
-            currentPage: 1,
-            searchChar:"",
-            newUserInfo:{
-                name:"",
-                username:"",
-                email:"",
-                address:"",
-            },
-            userIdToEdit: null,
-            getUsers(){
-                this.isLoading = true
-                axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
-                this.mainUsers = res.data
-                this.users = res.data
-                this.pagination()
-                }).finally(()=>{
-                    this.isLoading = false
+document.addEventListener("alpine:init", () => {
+    Alpine.data("usersData", () => ({
+        mainUsers: [],
+        users: [],
+        pageUsers: [],
+        isLoading: false,
+        showAdModal: false,
+        pageCount: 1,
+        itemsCount: 4,
+        currentPage: 1,
+        searchChar: "",
+        newUserInfo: {
+            name: "",
+            username: "",
+            email: "",
+            address: "",
+        },
+        userIdToEdit: null,
+        getUsers() {
+            this.isLoading = true;
+            axios
+                .get("https://jsonplaceholder.typicode.com/users")
+                .then((res) => {
+                    this.mainUsers = res.data;
+                    this.users = res.data;
+                    this.pagination();
                 })
-            },
-            pagination(){
-                this.pageCount = Math.ceil(this.users.length / this.itemsCount) // 10/4 = 3
-                let start = (this.currentPage * this.itemsCount) - this.itemsCount
-                let end = this.currentPage * this.itemsCount
-                this.pageUsers = this.users.slice(start,end)
-            },
-            nextPage(){
-                this.currentPage++
-                if (this.currentPage > this.pageCount) {
-                    this.currentPage = this.pageCount
-                }
-                this.pagination()
-            },
-            prevPage(){
-                this.currentPage--
-                if (this.currentPage < 1) {
-                    this.currentPage = 1
-                }
-                this.pagination()
-            },
-            handleChangeItemsCount(value){
-                this.currentPage = 1
-                if (value < 1) this.itemsCount = 1
-                if (value > this.users.length) this.itemsCount = this.users.length
-            },
-            handleSearch(value){
-                this.users = this.mainUsers.filter(user=>(user.name.includes(value) || user.username.includes(value) || user.email.includes(value)))
-                this.currentPage = 1;
-                this.pagination()
-            },
-            handleSubmitAddUserForm(){
-                this.isLoading = true
-                axios.post("https://jsonplaceholder.typicode.com/users", this.newUserInfo).then((res)=>{
-                    if (res.status == 201) {
-                        this.mainUsers.push(res.data)
-                        this.showAdModal = false
-                        this.handleResetForm()
-                        this.pagination()
-                        M.toast({html: 'User created successfully...!', classes: 'rounded green'});
-                    }
-                }).finally(()=>{
-                    this.isLoading = false
-                })
-            },
-            handleResetForm(){
-                this.newUserInfo = {
-                    name:"",
-                    username:"",
-                    email:"",
-                    address:"",
-                }
-            },
-            handleDeleteUser(userId){
-                var toastHTML = '<span>Are you sure? ('+userId+')</span><button class="btn-flat toast-action" x-on:click="handleConfirmDeleteUser('+userId+')">Delete</button>';
-                M.toast({html: toastHTML});
-            },
-            handleConfirmDeleteUser(userId){
-                this.isLoading = true
-                axios.delete("https://jsonplaceholder.typicode.com/users/"+userId).then((res)=>{
-                    if (res.status == 200) {
-                        this.mainUsers = this.mainUsers.filter(user=>user.id != userId)
-                        this.users = this.users.filter(user=>user.id != userId)
-                        this.pagination()
-                        M.toast({html: 'User deleted successfully...!', classes: 'green'})
-                    }
-                }).finally(()=>{
-                    this.isLoading = false
-                })
-            },
-            handleUpdateUser(user){
-                axios.get("https://jsonplaceholder.typicode.com/users/"+user.id).then(res=>{
-                    if (res.status == 200) {
-                        this.newUserInfo={
-                            name:res.data.name,
-                            username:res.data.username,
-                            email:res.data.email,
-                            address:res.data.address,
-                        }
-                        this.userIdToEdit = res.data.id
-                    }
-                })
-                this.showAdModal = true
-            },
-            handleConfirmEditUser(){
-                this.isLoading = false
-                axios.put("https://jsonplaceholder.typicode.com/users/"+this.userIdToEdit, this.newUserInfo).then(res=>{
-                    if (res.status == 200) {
-                        const userIndex = this.mainUsers.findIndex(user=>user.id == this.userIdToEdit)
-                        this.mainUsers[userIndex] = res.data
-                        this.showAdModal = false
-                        this.handleResetForm()
-                        this.userIdToEdit = null
-                        this.pagination()
-                        M.toast({html: 'User updated successfully...!', classes: 'green'})
-                    }
-                }).finally(()=>{
-                    this.isLoading = false
-                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+        pagination() {
+            this.pageCount = Math.ceil(this.users.length / this.itemsCount); // 10/4 = 3
+            let start = this.currentPage * this.itemsCount - this.itemsCount;
+            let end = this.currentPage * this.itemsCount;
+            this.pageUsers = this.users.slice(start, end);
+        },
+        nextPage() {
+            this.currentPage++;
+            if (this.currentPage > this.pageCount) {
+                this.currentPage = this.pageCount;
             }
-        }))
-})
+            this.pagination();
+        },
+        prevPage() {
+            this.currentPage--;
+            if (this.currentPage < 1) {
+                this.currentPage = 1;
+            }
+            this.pagination();
+        },
+        handleChangeItemsCount(value) {
+            this.currentPage = 1;
+            if (value < 1) this.itemsCount = 1;
+            if (value > this.users.length) this.itemsCount = this.users.length;
+        },
+        handleSearch(value) {
+            this.users = this.mainUsers.filter(
+                (user) =>
+                user.name.includes(value) ||
+                user.username.includes(value) ||
+                user.email.includes(value)
+            );
+            this.currentPage = 1;
+            this.pagination();
+        },
+        handleSubmitAddUserForm() {
+            this.isLoading = true;
+            axios
+                .post("https://jsonplaceholder.typicode.com/users", this.newUserInfo)
+                .then((res) => {
+                    if (res.status == 201) {
+                        this.mainUsers.push(res.data);
+                        this.showAdModal = false;
+                        this.handleResetForm();
+                        this.pagination();
+                        M.toast({
+                            html: "User created successfully...!",
+                            classes: "rounded green",
+                        });
+                    }
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+        handleResetForm() {
+            this.newUserInfo = {
+                name: "",
+                username: "",
+                email: "",
+                address: "",
+            };
+        },
+        handleDeleteUser(userId) {
+            var toastHTML =
+                "<span>Are you sure? (" +
+                userId +
+                ')</span><button class="btn-flat toast-action" x-on:click="handleConfirmDeleteUser(' +
+                userId +
+                ')">Delete</button>';
+            M.toast({
+                html: toastHTML,
+            });
+        },
+        handleConfirmDeleteUser(userId) {
+            this.isLoading = true;
+            axios
+                .delete("https://jsonplaceholder.typicode.com/users/" + userId)
+                .then((res) => {
+                    if (res.status == 200) {
+                        this.mainUsers = this.mainUsers.filter((user) => user.id != userId);
+                        this.users = this.users.filter((user) => user.id != userId);
+                        this.pagination();
+                        M.toast({
+                            html: "User deleted successfully...!",
+                            classes: "green",
+                        });
+                    }
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+        handleUpdateUser(user) {
+            axios
+                .get("https://jsonplaceholder.typicode.com/users/" + user.id)
+                .then((res) => {
+                    if (res.status == 200) {
+                        this.newUserInfo = {
+                            name: res.data.name,
+                            username: res.data.username,
+                            email: res.data.email,
+                            address: res.data.address.city + " - " + res.data.address.street,
+                        };
+                        this.userIdToEdit = res.data.id;
+                    }
+                });
+            this.showAdModal = true;
+        },
+        handleConfirmEditUser() {
+            this.isLoading = false;
+            axios
+                .put(
+                    "https://jsonplaceholder.typicode.com/users/" + this.userIdToEdit,
+                    this.newUserInfo
+                )
+                .then((res) => {
+                    if (res.status == 200) {
+                        const userIndex = this.mainUsers.findIndex(
+                            (user) => user.id == this.userIdToEdit
+                        );
+                        this.mainUsers[userIndex] = res.data;
+                        this.showAdModal = false;
+                        this.handleResetForm();
+                        this.userIdToEdit = null;
+                        this.pagination();
+                        M.toast({
+                            html: "User updated successfully...!",
+                            classes: "green",
+                        });
+                    }
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+    }));
+});
--- assets/js/users.js
+++ assets/js/users.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-    
-    // const addUserButton = document.getElementById('add_user_button')
-    // addUserButton.addEventListener('click', ()=>{
-    //   document.querySelector('.add_user_modal').classList.add('show')
-    //   document.querySelector('.add_user_modal_back').classList.remove('dis-none')
-    // })
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
 
-    // document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
-    //   document.querySelector('.add_user_modal').classList.remove('show')
-    //   e.target.classList.add('dis-none')
-    // })
+  // const addUserButton = document.getElementById('add_user_button')
+  // addUserButton.addEventListener('click', ()=>{
+  //   document.querySelector('.add_user_modal').classList.add('show')
+  //   document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+  // })
 
-  });
+  // document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+  //   document.querySelector('.add_user_modal').classList.remove('show')
+  //   e.target.classList.add('dis-none')
+  // })
+
+});
